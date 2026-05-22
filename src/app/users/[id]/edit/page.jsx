c39036a4969d8ce283ceec939439dccbb1c75fc7
ShafayatSadid <mdshafayathossainsadid@gmail.com>
2026-05-22 23:05:50 +0300

    add a form for updating user data

--- src/app/users/[id]/edit/page.jsx
+++ src/app/users/[id]/edit/page.jsx
@@ -1,9 +1,40 @@
+import { getUserById } from "@/lib/data";
+import { Button, Input, Label, TextField } from "@heroui/react";
 
-const UserEditPage = () => {
+const UserEditPage = async ({ params }) => {
+    const { id } = await params;
+    const user = await getUserById(id);
+    // console.log(user, 'User:..........');
     return (
         <div>
-           <h2>User Editing</h2> 
+            <h2>User Editing</h2>
+
+            <div className="w-1/4 mx-auto my-7">
+                <form className="flex flex-col gap-4">
+                <TextField className="w-full" defaultValue={user.name} name="name" type="text">
+                    <Label>Name</Label>
+                    <Input placeholder="Enter your name" />
+                </TextField>
+                <TextField className="w-full" name="email" type="email">
+                    <Label>Email</Label>
+                    <Input placeholder="Enter your email" />
+                </TextField>
+                <TextField className="w-full" name="role" type="text">
+                    <Label>Role</Label>
+                    <Input placeholder="Enter user role" />
+                </TextField>
+
+
+                <div className="flex gap-2">
+                    <Button slot="close" variant="secondary">
+                        Cancel
+                    </Button>
+                    <Button type="submit" slot="close">Update User</Button>
+                </div>
+
+            </form>
         </div>
+        </div >
     );
 };
 
